a5a95850b8489f8ba4d3c60ad0a876955c97f7b4
Marlon (Esolitos) Saglia <esolitos@gmail.com>
2021-12-10 14:31:56 +0100

    Cleanup code into a proper function.

--- ovh/scripts/post-install-default.sh
+++ ovh/scripts/post-install-default.sh
@@ -8,44 +8,55 @@ if [[ "-" != "${DEBUG:-}" ]]; then
   set -x
 fi
 
-MOUNTPOINT=/mnt/data
+main() {
+  setup_data_partition
+}
 
-# Ensure the mount directory is empty!
-if [[ $(find $MOUNTPOINT -type f | wc -l) -gt 0 ]]; then
-  echo "[ERROR] Mount directory '$MOUNTPOINT' is not empty!" 1>&2
-  exit 1
-fi
+setup_data_partition() {
+  local data_mount=/mnt/data
 
-# Ensure that we can detect the device.
-DEV_NAME="$(mount -l | grep "$MOUNTPOINT" | cut -d' ' -f1)"
-if [[ -z "$DEV_NAME" ]]; then
-  echo "[ERROR] Could not find the device for mount '$MOUNTPOINT'." 1>&2
-  exit 2
-fi
+  # Ensure the mount directory is empty!
+  if [[ $(find "$data_mount" -type f | wc -l) -gt 0 ]]; then
+    echo "[ERROR] Mount directory '${data_mount}' is not empty!" 1>&2
+    exit 1
+  fi
 
-# Store the old UUID to replace it in fstab.
-OLD_UUID=$(blkid -o value -s UUID $DEV_NAME)
+  # Ensure that we can detect the device.
+  local block_dev_name=$(mount -l | grep "${data_mount}" | cut -d' ' -f1)
+  if [[ -z "$block_dev_name" ]]; then
+    echo "[ERROR] Could not find the device for mount '${data_mount}'." 1>&2
+    exit 2
+  fi
 
-# Reformat the partition with increased i-node count.
-umount "$MOUNTPOINT"
-mkfs.ext4 -F -i 2048 $DEV_NAME
+  # Store the old UUID to replace it in fstab.
+  local old_dev_uuid=$(blkid -o value -s UUID "${block_dev_name}")
 
-NEW_UUID=$(blkid -o value -s UUID $DEV_NAME)
-sed --in-place -e "s/$OLD_UUID/$NEW_UUID/" /etc/fstab
+  # Reformat the partition with increased i-node count.
+  umount "${data_mount}"
+  mkfs.ext4 -F -i 2048 "${block_dev_name}"
 
-# Reload config and re-mount the disk.
-systemctl daemon-reload 
-mount -a
+  local new_dev_uuid=$(blkid -o value -s UUID "${block_dev_name}")
+  sed --in-place -e "s/${old_dev_uuid}/${new_dev_uuid}/" /etc/fstab
 
-# Check if mountpoint is re-mounted.
-if [[ -z "$(mount  -l | grep $MOUNTPOINT )" ]]; then
-  echo "[ERROR] Re-mount error: '$MOUNTPOINT' did not auto-mount." 1>&2
-  exit 3
-fi
+  # Reload config and re-mount the disk.
+  systemctl daemon-reload 
+  mount -a
 
-# Create symlinks for docker and wodby.
-mkdir "$MOUNTPOINT/var-lib-docker" "$MOUNTPOINT/srv-wodby"
-ln -s "$MOUNTPOINT/var-lib-docker" /var/lib/docker
-ln -s "$MOUNTPOINT/srv-wodby" /srv/wodby
+  # Check if data_mount is re-mounted.
+  if [[ -z "$(mount  -l | grep $data_mount )" ]]; then
+    echo "[ERROR] Re-mount error: '${data_mount}' did not auto-mount." 1>&2
+    exit 3
+  fi
 
-# All done.
+  # Create symlinks for docker and wodby.
+  mkdir "${data_mount}/var-lib-docker" "${data_mount}/srv-wodby"
+  ln -s "${data_mount}/var-lib-docker" /var/lib/docker
+  ln -s "${data_mount}/srv-wodby" /srv/wodby
+  # All done.
+}
+
+
+#
+# Actual script: main function
+#
+main
